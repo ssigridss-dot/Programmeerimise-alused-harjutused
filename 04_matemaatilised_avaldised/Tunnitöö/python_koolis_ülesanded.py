30f2dd0144c21730e2c73957ac194415fac8ec9d
Sigrid <ssigridss@gmail.com>
2025-12-16 11:31:14 +0200

    Tunnitöö python koolis ülesanded(tsükkel)

--- 04_matemaatilised_avaldised/Tunnitöö/python_koolis_ülesanded.py
+++ 04_matemaatilised_avaldised/Tunnitöö/python_koolis_ülesanded.py
@@ -71,6 +71,36 @@ def dog_calculate(num1: float, num2: float, operation: str) -> str:
     return f"{round(result)*"auh"}"
 
 
+def convert_to_fahrenheit(celsius_temperature: float) -> float:
+    """Koosta programm, mis küsib kasutajalt temperatuuri Celsiuse
+    kraadides ja väljastab tulemuse Fahrenheiti kraadides. Kuidas muuta programmi nii,
+    et võimalik oleks teisendamine nii üht- kui teistpidi?"""
+    return celsius_temperature * 1.8 + 32
+
+
+def convert_to_celsius(fahrenheit_temperature: float) -> float:
+    """Convert given Fahrenheit temperature to Celsius"""
+    return (fahrenheit_temperature - 32) / 1.8
+
+
+def calculate_discriminant(a: float, b: float, c: float) -> float:
+    """Loo programm, mis küsib kasutajalt ruutvõrrandi liikmete (ruutliige,
+    lineaarliige, vabaliige) kordajad ning arvutab nende põhjal diskriminandi ja
+    väljastab selle põhjal ruutvõrrandi lahendid. Nagu tead, võib lahendeid vastavalt diskriminandi
+    väärtusele olla üks või kaks, kuid lahendid võivad ka puududa."""
+    return b ** 2 - 4 * a * c
+
+
+def solve_quadratic_equation(a, b, useAddition):
+    import math
+    if useAddition:
+        top = -b + math.sqrt(discriminant)
+    else:
+        top = -b - math.sqrt(discriminant)
+    bottom = 2 * a
+    return top / bottom
+
+
 if __name__ == '__main__':
     compute_rectangle()
 
@@ -84,3 +114,35 @@ if __name__ == '__main__':
     num2 = int(input("Sisestage teine arv:"))
     operation = input("Sisestage tehe: ")
     print(f"Vastus: {calculate(num1, num2, operation)}")
+
+    unit = input("Määra sisestatava temperatuuri ühik (C/F): ")
+    if unit.upper() == "C":
+        temperature_celsius = float(input("Sisesta temperatuur Celsiuse kraadides: "))
+        temperature_fahrenheit = convert_to_fahrenheit(temperature_celsius)
+        print(f"{temperature_celsius}C on {temperature_fahrenheit:.2f}F kraadi.")
+    elif unit.upper() == "F":
+        temperature_fahrenheit = float(input("Sisesta temperatuur Fahrenheiti kraadides: "))
+        temperature_celsius =  convert_to_celsius(temperature_fahrenheit)
+        print(f"{temperature_fahrenheit}F on {temperature_celsius:.2f}C kraadi.")
+    else:
+        print(f"Sisestasid tundmatu temperatuuriühiku - {unit}")
+        print("Programm toetab C - Celsius ja F - Fahrenheiti kraade.")
+
+
+    print("Arvutame ruutvõrrandit!")
+    a = float(input("Sisesta ruutliige: "))
+    if a == 0:
+        print("Ruutliige ei tohi olla null.")
+    else:
+        b = float(input("Sisesta lineaarliige: "))
+        c = float(input("Sisesta vabaliige: "))
+        discriminant = calculate_discriminant(a, b, c)
+        if discriminant < 0:
+            print("Lahendid puuduvad")
+        elif discriminant == 0:
+            solution = solve_quadratic_equation(a, b, discriminant, True)
+            print(f"Lahendid on võrdsed: {solution}")
+        else:
+            solution1 = solve_quadratic_equation(a, b, discriminant, True)
+            solution2 = solve_quadratic_equation(a, b, discriminant, False)
+            print(f"Lahendeid on kaks: {solution1} ja {solution2}")
