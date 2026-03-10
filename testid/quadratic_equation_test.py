c928847993da1c2b6584e7c7677a4321d370a247
Opilane TTHK <opilane@tthk.ee>
2026-03-10 11:52:51 +0200

    tunnitöö + KT2

---
+++ testid/quadratic_equation_test.py
@@ -0,0 +1,7 @@
+from quadratic_eqation import solve_quadratic_equation as solve
+
+def test_integer_values():
+    assert solve(1, -3, 2) == (1, 2)
+
+def test_float_values():
+    assert solve(1, -4, )
